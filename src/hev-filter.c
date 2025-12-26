3799191c0196255a1eddce4af2d1c3de5f4019dd
yiguihai11 <yiguihai11@users.noreply.github.com>
2025-12-26 13:26:41 +0800

    fix: add BSD_SOURCE and sys/types.h for FreeBSD IPv6 support

--- src/hev-filter.c
+++ src/hev-filter.c
@@ -7,6 +7,8 @@
  ============================================================================
  */
 
+#define _BSD_SOURCE
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
